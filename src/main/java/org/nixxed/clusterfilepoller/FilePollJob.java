e8c9b7ba3c3956e2cc4c0c7d1f5ef9c42c322267
Andy Christianson <nix@nixxed.com>
2014-11-12 21:56:06 -0600

    style(cfp): Formatting.

--- src/main/java/org/nixxed/clusterfilepoller/FilePollJob.java
+++ src/main/java/org/nixxed/clusterfilepoller/FilePollJob.java
@@ -4,41 +4,41 @@ package org.nixxed.clusterfilepoller;
  * Describe a job for the cluster file poller. Will create one timer job / thread.
  */
 public class FilePollJob {
-	private String path;
-	private String regex;
-	private int intervalSeconds;
+    private String path;
+    private String regex;
+    private int intervalSeconds;
     private FilePollListener listener;
 
-	public FilePollJob(String path, String regex, int intervalSeconds, FilePollListener listener) {
-		this.path = path;
-		this.regex = regex;
-		this.intervalSeconds = intervalSeconds;
+    public FilePollJob(String path, String regex, int intervalSeconds, FilePollListener listener) {
+        this.path = path;
+        this.regex = regex;
+        this.intervalSeconds = intervalSeconds;
         this.listener = listener;
-	}
+    }
 
-	public String getPath() {
-		return path;
-	}
+    public String getPath() {
+        return path;
+    }
 
-	public void setPath(String path) {
-		this.path = path;
-	}
+    public void setPath(String path) {
+        this.path = path;
+    }
 
-	public String getRegex() {
-		return regex;
-	}
+    public String getRegex() {
+        return regex;
+    }
 
-	public void setRegex(String regex) {
-		this.regex = regex;
-	}
+    public void setRegex(String regex) {
+        this.regex = regex;
+    }
 
-	public int getIntervalSeconds() {
-		return intervalSeconds;
-	}
+    public int getIntervalSeconds() {
+        return intervalSeconds;
+    }
 
-	public void setIntervalSeconds(int intervalSeconds) {
-		this.intervalSeconds = intervalSeconds;
-	}
+    public void setIntervalSeconds(int intervalSeconds) {
+        this.intervalSeconds = intervalSeconds;
+    }
 
     public FilePollListener getListener() {
         return listener;
